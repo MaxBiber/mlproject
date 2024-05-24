0e9bb1e5e95f2b8ff9a34b84ab317e0f0e99ea23
Max Biber <max.biber@orange.fr>
2024-05-24 09:54:25 +0200

    add standardScaler

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -48,7 +48,8 @@ class DataTransformation:
 
                 steps=[
                 ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("one_hot_encoder",OneHotEncoder())
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler", StandardScaler(with_mean=False))
                 ]
 
             )
